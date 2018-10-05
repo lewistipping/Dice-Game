8acebfaf685654d078d5eba0c474e9aa0e6b0b13
Lewis Tipping <lewistipping01@hotmail.co.uk>
2018-10-05 15:51:53 +0100

    Initial Commit

---
+++ Player.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Game
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public double cash { get; set; }
+        public double chips { get; set; }
+
+        public Player()
+        {
+            this.name = "Default Name";
+            this.cash = 0.0;
+            this.chips = 0.0;
+        }
+        public Player(string name, double cash)
+        {
+            this.name = name;
+            this.cash = cash;
+            this.chips = 0.0;
+        }
+        public string displayDetails()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name: " + this.name + "\n");
+            sb.Append("Cash Balance: £" + this.cash + "\n");
+            sb.Append("Chip Balance: £" + this.chips + "\n");
+
+            return sb.ToString();
+
+        }
+        public void purchaseChips(double chips)
+        {
+            if (this.cash < chips)
+            {
+                Console.WriteLine("Unable to purchase chips,insufficient funds.");
+            }
+            else
+            {
+                this.cash -= chips;
+                this.chips += chips;
+            }
+        }
+        public void cashInChips(double chips)
+        {
+            if (this.chips < chips)
+            {
+                Console.WriteLine("You do not have this many chips");
+            }
+            else
+            {
+                this.cash += chips;
+                this.chips -= chips;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,85 @@ namespace Dice_Game
     {
         static void Main(string[] args)
         {
+            Player pl = new Player("Lewis", 200);
+            while (true)
+            {
+
+                Console.WriteLine("Choose from one of the following options\n1:Place a bet.\n2:See account details."
+                                  + "\n3:Purchase Chips.\n4:Cash In Chips.");
+
+                int input = Int32.Parse(Console.ReadLine());
+
+                int selection = 0;
+                int stake = 0;
+                int diceRoll = 0;
+                switch (input)
+                {
+                    case 1:
+
+                        if (pl.chips <= 0)
+                        {
+                            Console.WriteLine("Insufficient chips to place bet, please purchase more to play.");
+                            break;
+                        }
+                        while (true)
+                        {
+                            Console.WriteLine("Choose a die roll to bet on.");
+                            selection = Int32.Parse(Console.ReadLine());
+                            if (selection >= 1 && selection <= 6)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error, please make sure you enter a number between 1 and 6.");
+                            }
+                        }
+                        while (true)
+                        {
+                            Console.WriteLine("Enter Stake: ");
+                            stake = Int32.Parse(Console.ReadLine());
+                            if (stake < pl.chips)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please enter an amount equal to or less than your chips balance");
+                            }
+                        }
+                        Random rnd = new Random();
+                        diceRoll = rnd.Next(1, 6);
+                        Console.WriteLine("The die rolled :" + diceRoll);
+
+                        if (selection == diceRoll)
+                        {
+                            pl.chips += stake;
+                            Console.WriteLine("Congratulations you've won\nNew balance is :£" + pl.chips);
+                        }
+                        else
+                        {
+                            pl.chips -= stake;
+                            Console.WriteLine("Unlucky please try again.");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine(pl.displayDetails());
+                        break;
+                    case 3:
+                        Console.WriteLine("How many chips would you like to purchase?");
+                        int purchaseAmount = Int32.Parse(Console.ReadLine());
+                        pl.purchaseChips(purchaseAmount);
+                        break;
+                    case 4:
+                        Console.WriteLine("How many chips would you like to cash in?");
+                        int cashInAmount = Int32.Parse(Console.ReadLine());
+                        pl.cashInChips(cashInAmount);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
